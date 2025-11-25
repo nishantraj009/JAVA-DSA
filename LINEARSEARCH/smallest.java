afaa6097a203cec8340560e4b6474db9c5fb9d82
nishant <nishantraj2052004@gmail.com>
2025-11-25 18:44:25 +0530

    fixed

--- LINEARSEARCH/smallest.java
+++ LINEARSEARCH/smallest.java
@@ -1,11 +1,18 @@
 public class smallest {
     public static int SmallestNum(int num []){
+        int smallest = Integer.MAX_VALUE;
         for(int i=0;i<num.length;i++){
+            if(num[i] < smallest ){
+                smallest = num[i];
+            }
              
         } 
+        return smallest;
     }
     public static void main(String[] args){
        int num [] = {20,3,4,25,34,23,8};
+       int smallvalue = SmallestNum(num);
+       System.out.print("smallest number is : " +smallvalue);
 
     }
 }

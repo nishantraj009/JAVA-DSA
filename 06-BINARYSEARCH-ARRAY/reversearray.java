618becd9d81753564912b158fce0679695afd1ed
nishant <nishantraj2052004@gmail.com>
2025-11-30 09:20:26 +0530

    fixed

---
+++ 06-BINARYSEARCH-ARRAY/reversearray.java
@@ -0,0 +1,38 @@
+
+
+public class reversearray {
+
+    public static void reverse(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            // Swap arr[start] and arr[end]
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+
+        System.out.print("Original Array: ");
+        printArray(arr);
+
+        reverse(arr);
+
+        System.out.print("Reversed Array: ");
+        printArray(arr);
+    }
+}

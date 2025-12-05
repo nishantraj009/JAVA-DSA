e6b2ad443910f9b35f150cb338c0e9c0079ef55e
nishant <nishantraj2052004@gmail.com>
2025-12-05 22:57:13 +0530

    fixed

--- 03-PATTERN/floydtriangle.java
+++ 03-PATTERN/floydtriangle.java
@@ -1,19 +1,21 @@
 public class floydtriangle {
     public static void main(String[] args) {
 
-        int n = 5;   // number of rows
-        int num = 1; // starting number
+        int n = 5;      // number of rows
+        int num = 1;    // starting number
 
         // outer loop  rows
+
         for(int i = 1; i <= n; i++) {
 
-            // inner loop  print elements in each row
+            // inner loop print elements in each row
+
             for(int j = 1; j <= i; j++) {
                 System.out.print(num + " ");
-                num++; // increase number
+                num++;      // increase number
             }
 
-            System.out.println(); // next line
+            System.out.println();     // next line
         }
     }
 }

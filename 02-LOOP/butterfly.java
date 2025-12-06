24a2b0b4d2a8acc9c277de68101c0b5b46b779ff
nishant <nishantraj2052004@gmail.com>
2025-12-06 22:17:46 +0530

    fixed code

--- 02-LOOP/butterfly.java
+++ 02-LOOP/butterfly.java
@@ -21,15 +21,15 @@ public class butterfly {
         
         
         for(int i=n; i>=1; i--) {
-            // Left Stars
+            
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
             }
-            // Spaces
+            
             for(int j=1; j<=2*(n-i); j++) {
                 System.out.print(" ");
             }
-            // Right Stars
+            
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
             }

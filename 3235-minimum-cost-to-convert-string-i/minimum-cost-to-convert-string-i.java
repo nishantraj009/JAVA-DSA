905bdcd9908e31b60872bb9912dd4ed014470da3
Nishant Raj <nishantraj2052004@gmail.com>
2026-01-29 13:37:37 +0530

    Time: 21 ms (72.40%) | Memory: 48 MB (65.59%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -5,20 +5,20 @@ class Solution {
         int INF = (int)1e9;
         int[][] dist = new int[26][26];
 
-        // init
+       
         for (int i = 0; i < 26; i++) {
             Arrays.fill(dist[i], INF);
             dist[i][i] = 0;
         }
 
-        // edges
+        
         for (int i = 0; i < original.length; i++) {
             int u = original[i] - 'a';
             int v = changed[i] - 'a';
             dist[u][v] = Math.min(dist[u][v], cost[i]);
         }
 
-        // Floyd-Warshall
+       
         for (int k = 0; k < 26; k++) {
             for (int i = 0; i < 26; i++) {
                 for (int j = 0; j < 26; j++) {
@@ -29,7 +29,7 @@ class Solution {
             }
         }
 
-        // calculate total cost
+        
         long ans = 0;
         for (int i = 0; i < source.length(); i++) {
             int s = source.charAt(i) - 'a';

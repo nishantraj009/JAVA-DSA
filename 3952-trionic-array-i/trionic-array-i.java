0fa8f6728b1c80ea5226c91cc15dffd29e64ac7f
Nishant Raj <nishantraj2052004@gmail.com>
2026-02-03 14:33:51 +0530

    Time: 1 ms (72.27%) | Memory: 44.7 MB (49.58%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -4,20 +4,18 @@ class Solution {
         if (n < 3) return false;
 
         int i = 0;
-
-        // 1️⃣ strictly increasing
+        
         while (i + 1 < n && nums[i] < nums[i + 1]) {
             i++;
         }
         if (i == 0 || i == n - 1) return false;
-
-        // 2️⃣ strictly decreasing
+        
         while (i + 1 < n && nums[i] > nums[i + 1]) {
             i++;
         }
         if (i == n - 1) return false;
 
-        // 3️⃣ strictly increasing again
+        
         while (i + 1 < n && nums[i] < nums[i + 1]) {
             i++;
         }

280d5d7cac364f2fca89a2bde0dd009d8d88dc20
Nishant Raj <nishantraj2052004@gmail.com>
2026-03-16 09:59:29 +0530

    Time: 94 ms (29.41%) | Memory: 48.9 MB (19.12%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
@@ -8,30 +8,29 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
 
-                // k = 0 (single cell)
+               
                 set.add(grid[i][j]);
 
-                // try all sizes
+                
                 for (int k = 1; ; k++) {
                     if (i - k < 0 || i + k >= m || j - k < 0 || j + k >= n)
                         break;
 
                     int sum = 0;
 
-                    // 4 edges
-                    // top → right
+                    
                     for (int t = 0; t < k; t++)
                         sum += grid[i - k + t][j + t];
 
-                    // right → bottom
+                   
                     for (int t = 0; t < k; t++)
                         sum += grid[i + t][j + k - t];
 
-                    // bottom → left
+                    
                     for (int t = 0; t < k; t++)
                         sum += grid[i + k - t][j - t];
 
-                    // left → top
+                    
                     for (int t = 0; t < k; t++)
                         sum += grid[i - t][j - k + t];
 

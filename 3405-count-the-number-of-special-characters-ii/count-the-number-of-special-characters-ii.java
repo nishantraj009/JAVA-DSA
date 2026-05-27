476cd20dc69c685f8b13b98daa59d18d616827fd
Nishant Raj <nishantraj2052004@gmail.com>
2026-05-27 21:39:13 +0530

    Time: 21 ms (66.94%) | Memory: 48.4 MB (22.31%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -0,0 +1,42 @@
+
+
+/*********************************************************** JAVA **************************************************/
+//Approach(Track last occurrence of lowercase and first occurrence of uppercase)
+//T.C : O(n)
+//S.C : O(1) - fixed-size arrays of length 26
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        
+        int[] lastSmall = new int[26];
+        int[] firstCapital = new int[26];
+
+        java.util.Arrays.fill(lastSmall, -1);
+        java.util.Arrays.fill(firstCapital, -1);
+
+        for(int i = 0; i < word.length(); i++) {
+            
+            char ch = word.charAt(i);
+
+            if(Character.isLowerCase(ch)) {
+                lastSmall[ch - 'a'] = i;
+            }
+            else {
+                if(firstCapital[ch - 'A'] == -1) {
+                    firstCapital[ch - 'A'] = i;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for(int i = 0; i < 26; i++) {
+
+            if(lastSmall[i] != -1 && firstCapital[i] != -1 &&
+               lastSmall[i] < firstCapital[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
